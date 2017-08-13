af29c73ba88e362e62c1cd01e0f56de642aa1668
Alex <alex@Alexs-MacBook-Pro.local>
2017-08-13 00:15:24 -0400

    full CRUD with mongoose!

--- config.js
+++ config.js
@@ -2,8 +2,14 @@ const env = process.env;
 
 //export const nodeEnv = env.NODE_ENV || 'development';
 
+
+var mgp = "lakjsdf0898Y987typuih%967tuy$E^%*R&FTG*YUHIO&6re5423$#65d";
+var mus = "npp";
+
 export default {
-    mongodbUri: "mongodb://localhost:27017/test",
+    mgp:"lakjsdf0898Y987typuih%967tuy$E^%*R&FTG*YUHIO&6re5423$#65d",
+    mus:"npp",
+    mongodbUri: 'mongodb://localhost:27017/test',
     port: env.PORT || 8080,
     host: env.HOST || "0.0.0.0",
     get serverUrl(){
---
+++ models/item.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+import config from '../config';
+
+let db;
+//connect
+mongoose.connect(config.mongodbUri,{user:config.mus,pwd:config.mgp,useMongoClient:true});
+mongoose.Promise = global.Promise
+db = mongoose.connection;
+db.on("error",console.error.bind(console,'connection error w/ mongoose....'));
+db.once('openUri',()=>{console.log("Mongo is now connected via mongoose.")});
+
+
+var itemSchema = mongoose.Schema({title:String,content:String});
+var Item = mongoose.model('Item',itemSchema);
+
+export default Item;
